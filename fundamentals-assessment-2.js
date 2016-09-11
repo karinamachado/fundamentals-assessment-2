f1e28cb35893aee1ceb350a4f840e83ad73b9831
Karina Machado <karinamachado@msn.com>
2016-09-11 16:51:44 -0300

    update exercise

--- fundamentals-assessment-2.js
+++ fundamentals-assessment-2.js
@@ -2,3 +2,62 @@ var two = 2;
 var myArray = [1, two, "three", "4", 5, "six", "7", 8];
 
 //YOUR CODE BELOW
+
+var printAll = function(arrArg){
+
+
+	for ( var i = 0; i < myArray.length; i++){
+
+		console.log(arrArg(myArray[i]);
+	}
+}
+
+printAll(myArray);
+
+
+
+var longArray = function(x){
+
+    
+	if(myArray[i] >= myArray.length){
+
+		if(x.length >= 7)
+		return true;
+
+
+	}else{
+		return false;
+	}
+
+
+}
+
+console.log(longArray()(myArray));
+
+
+
+
+var countNums = function(chocolate){
+  
+  var counter = 0;
+
+  for(var i = 0; i > chocolate.length; i++){
+
+  	if( typeof === "number"){
+
+  		
+  	}
+
+
+  }
+
+  return counter;
+
+
+
+}
+
+
+
+
+
